b1d98dab308de5858ddfa2f26c8ba984d0217c23
Anna Harder <annaqharder@gmail.com>
2022-11-14 14:26:24 -0700

    render back card

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ fetch("https://zoo-animal-api.herokuapp.com/animals/rand/10")
     .then(animals=> {
         animals.forEach(oneAnimal => {
             renderFrontCard(oneAnimal)
+            renderBackCard(oneAnimal)
         })
 
         console.log(animals)
@@ -30,7 +31,26 @@ function renderFrontCard(animal){
 
     // })
 }
-
+function renderBackCard(animals) {
+    const latinName = document.querySelector("#latin-name");
+    latinName.textContent = `Latin Name: ${animals.latin_name}`;
+    const type = document.querySelector("#type")
+    type.textContent = `Type: ${animals.animal_type}`;
+    const location = document.querySelector("#location")
+    location.textContent = `Location: ${animals.geo_range}`;
+    const habitat = document.querySelector("#habitat")
+    habitat.textContent = `Habitat: ${animals.habitat}`;
+    const diet = document.querySelector("#diet")
+    diet.textContent = `Diet: ${animals.diet}`;
+    const lifespan = document.querySelector("#lifespan")
+    lifespan.textContent = `Lifespan: ${animals.lifespan}`;
+    const activeTime = document.querySelector("#active-time")
+    activeTime.textContent = `Active Time: ${animals.active_time}`;
+    const maxWeight = document.querySelector("#max-weight")
+    maxWeight.textContent = `Max Weight: ${animals.weight_max}`;
+    const maxLength = document.querySelector("#max-length")
+    maxLength.textContent = `Max Length: ${animals.length_max}`;
+}
 
 
 
