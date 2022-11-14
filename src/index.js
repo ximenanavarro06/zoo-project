bcbf2853f589db3ff28948e19ac53475a63ae61c
Anna Harder <annaqharder@gmail.com>
2022-11-14 12:52:52 -0700

    front card HTML

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ fetch("https://zoo-animal-api.herokuapp.com/animals/rand/10")
         console.log(data)
     })
 
+// function spottedFunction(){
+//     const checkBox = document.querySelector("#myCheck");
+//     const text = document.querySelector("#display-text");
+
+//     if (checkBox.checked == true){
+//         text.style.dislay = "block"; 
+//     } else { 
+//         text.style.display = "none"
+//     }
+// }
